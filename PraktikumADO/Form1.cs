f7c4824a970a0628780927957c6a9095d5c49442
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2026-03-31 22:00:46 +0800

    menambahkan catch-try pada koneksi database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -170,7 +170,10 @@ namespace PraktikumADO
                 conn.Close();
 
             }
-            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
         

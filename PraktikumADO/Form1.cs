7a40f0b0d63f166449601eeaf2da01e9c5ec1d99
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2026-03-31 20:57:50 +0800

    menambahkan fitur hitung jumlah mahasiswa

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -48,5 +48,21 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+            }
+            
+        }
     }
 }

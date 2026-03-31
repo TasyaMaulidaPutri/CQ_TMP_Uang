8d8150564f1dd02acab4cf7ecd79c22e158fa95a
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2026-03-31 20:38:35 +0800

    menambahkan method koneksi database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -16,12 +16,23 @@ namespace PraktikumADO
         SqlConnection conn;
         SqlCommand cmd;
 
+        private void koneksi ()
+        {
+            conn = new SqlConnection(
+                "Data Source =LAPTOP-7BCU6RBN\\TASYAMAULIDA;Initial Catalog=DBAkademikADO;Intergrated Security=True"
+                );
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
